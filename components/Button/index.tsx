fd536d4394ab11b86bd41f94c462ae07753592ec
ArtiomB5 <artiomb5@yandex.by>
2022-10-08 20:50:49 +0600

    button and htag components typing

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,11 +2,11 @@ import styles from './Button.module.scss';
 import classNames from 'classnames';
 import { IButtonProps } from './Button.props';
 
-export const Button = ({ appearance, children, handler }: IButtonProps): JSX.Element => {
+export const Button = ({ appearance, children, handler, ...props }: IButtonProps): JSX.Element => {
     const className = classNames(styles.button, {
         [styles.primary]: appearance === "primary",
         [styles.ghost]: appearance === "ghost",
       });
 
-    return <button onClick={handler} className={className}>{children}</button>;
+    return <button onClick={handler} className={className} {...props}>{children}</button>;
 };
